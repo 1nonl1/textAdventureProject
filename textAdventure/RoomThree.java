e1b42e16d4aa53009f7081edc6d5492948393e59
1nonly <tobiasbraymen@gmail.com>
2025-09-05 14:07:28 +0000

    git commit

---
+++ textAdventure/RoomThree.java
@@ -0,0 +1,52 @@
+//package textAdventure;
+//Toby
+import java.util.Scanner;
+
+class RoomThree extends Room{
+	protected int roomNum;
+	public RoomThree(int roomNum) {
+		this.roomNum = roomNum;
+	}
+	public int changeRoom(int choice){
+		int switchToRoom = 0;
+		switch(choice) {
+			case 1:
+				switchToRoom = 4;
+				break; // Room to change to
+			case 2:
+				switchToRoom = 7;
+				break;
+			case 3:
+				switchToRoom = 8;
+				break;
+			default:
+				System.err.println("Not a choice!");
+				break;
+		}
+		return switchToRoom;
+	}
+	public void showProblem(){
+	//Custom problem for rooms (Define Here) 
+        System.out.println("Upon answering the question, you see three different hallways.");
+        System.out.println("The first hallway, you again hear the familiar sound of water.");
+        System.out.println("The second hallway is completly dark.");
+        System.out.println("The next hallway has a door (No lock) preventing you from going any farther.");
+        System.out.println("What will you do? \n\t1)Go towards the water (Room 4)\n\t2)Explore the dark hallway (Room 7)\n\t3)Open the door to see what is on the other side (Room 8");
+	}
+	public void roomInteraction(Player other){
+        System.out.println("You decide to go to the light, you see another riddle written on the wall.");
+        for(int i = 3; i >= 0; i--){
+            System.out.println("It belongs to you, but other people use it more than you do. What is it?"); //Question
+            Scanner scan = new Scanner(System.in);
+            String answer = scan.nextLine();
+            if(answer.toLowerCase().contains("name")){ // Answer
+                System.out.println("You have solved the riddle!");
+                break;
+        }
+        else{
+            System.out.println("You have " + i + " tries left.");
+        }
+    } 
+    System.out.println("You died!");	
+    }	
+}
---
+++ textAdventure/RoomTwo.java
@@ -0,0 +1,40 @@
+//package textAdventure;
+//Toby
+
+class RoomTwo extends Room{
+	protected int roomNum;
+	public RoomTwo(int roomNum) {
+		this.roomNum = roomNum;
+	}
+	public int changeRoom(int choice){
+		int switchToRoom = 0;
+		switch(choice) {
+			case 1:
+				switchToRoom = 1;
+				break; // Room to change to
+			case 2:
+				switchToRoom = 5;
+				break;
+			case 3:
+				switchToRoom = 6;
+				break;
+			default:
+				System.err.println("Not a choice!");
+				break;
+		}
+		return switchToRoom;
+	}
+	public void showProblem(){
+	//Custom problem for rooms (Define Here) 
+	System.out.println("After defeating the boss, two rooms appear in front of you. And the door you came from unlocks.");
+    System.out.println("You want to go back to room 1 and recouperate");
+    System.out.println("But, you see the first room have a pile of bones.");
+    System.out.println("And the next room have a chest, but you suspect there might be a trap.");
+	System.out.println("What will you do? \n\t1)Go back to saftey (Room 1)\n\t2)Inspect the pile of bones (Room 5)\n\t3)Try your luck for the chest (Room 6");
+
+	}
+	public void roomInteraction(Player other){
+		System.out.println("You follow the breeze and you enter room two. The door suddenly shuts and you are greeted by a monster that attacks you.");
+        other.battle(new Monster(2, 5, "Slime", "eerily squishy"));		
+	}	
+}
